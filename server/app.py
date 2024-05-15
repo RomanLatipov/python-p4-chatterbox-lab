595e0ce81931e9d7381032562046bc8ce41d3e87
romanlatipov <romanlatipov@gmail.com>
2024-05-15 14:42:34 -0400

    complete chatterbox-lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,45 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+
+@app.get('/messages')
+def get_rqst():
+    messages = [msg.to_dict() for msg in Message.query.order_by(Message.created_at).all()]
+    return messages, 200
+
+@app.post('/messages')
+def post_rqst():
+    message = Message(body=request.json['body'], username=request.json['username'])
+    db.session.add(message)
+    db.session.commit()
+    return message.to_dict(), 201
+
+@app.patch('/messages/<int:id>')
+def patch_rqst(id):
+    message = Message.query.where(Message.id == id).first()
+    if message:
+        for key in request.json.keys():
+            if not key =='id':
+                setattr(message, key, request.json[key])
+
+        db.session.add(message)
+        db.session.commit()
+
+        return message.to_dict(), 202
+    
+    else:
+        return {"error": "Not found"}, 404
+    
+@app.delete('/messages/<int:id>')
+def delete_rqst(id):
+    message = Message.query.where(Message.id == id).first()
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return {}, 204
+    else:
+        return {"error": "Not found"}, 404
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
